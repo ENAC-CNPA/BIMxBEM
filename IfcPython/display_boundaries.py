130e43091bd3b302f41dd49deeb85411718c3827
cyril@biminsight.ch <UwLLxEzxXeBoEvnVzztk>
2019-07-17 03:05:17 +0200

    Add placement and/or coloring

--- IfcPython/display_boundaries.py
+++ IfcPython/display_boundaries.py
@@ -1,46 +1,76 @@
 # coding: utf8
+"""This module display IfcRelSpaceBoundary with standard ifcopenshell viewer"""
 import ifcopenshell
 import ifcopenshell.geom
-import OCC.BRepBuilderAPI
 import OCC.BRep
+import OCC.BRepBuilderAPI
 import OCC.gp
 import OCC.TopoDS
 
-ifc_path = "IfcPython/9000_BIMxBEM_TestModèle_ACAD.ifc"
-ifc_file = ifcopenshell.open(ifc_path)
 
-space_boundaries = ifc_file.by_type('IfcRelSpaceBoundary')
-s = ifcopenshell.geom.settings()
-# s.set(s.USE_PYTHON_OPENCASCADE, True)
-# s.set(s.USE_WORLD_COORDS, True)
-s.set(s.EXCLUDE_SOLIDS_AND_SURFACES, False)
-s.set(s.INCLUDE_CURVES, True)
+def display_boundaries(ifc_path):
+    """Display all IfcRelSpace Boundary from file"""
+    ifc_file = ifcopenshell.open(ifc_path)
 
-occ_display = ifcopenshell.geom.utils.initialize_display()
-product_shapes = []
-for boundary in (b for b in space_boundaries if b.Name == "2ndLevel"):
-    # Retrieve boundary wire    
-    geom = ifcopenshell.geom.create_shape(s, boundary.ConnectionGeometry.SurfaceOnRelatingElement.OuterBoundary) # type: OCC.TopoDS.Compound
-    v = geom.verts
-    verts = [OCC.gp.gp_Pnt(v[i], v[i+1], v[i+2]) for i in range(0, len(v), 3)]
-    edges = [OCC.BRepBuilderAPI.BRepBuilderAPI_MakeEdge(verts[i], verts[i+1]).Edge() for i in range(0, len(verts), 2)]
-    w = OCC.BRepBuilderAPI.BRepBuilderAPI_MakeWire()
+    space_boundaries = ifc_file.by_type("IfcRelSpaceBoundary")
+    settings = ifcopenshell.geom.settings()
+    # s.set(s.USE_PYTHON_OPENCASCADE, True)
+    # s.set(s.USE_WORLD_COORDS, True)
+    settings.set(settings.EXCLUDE_SOLIDS_AND_SURFACES, False)
+    settings.set(settings.INCLUDE_CURVES, True)
+
+    occ_display = ifcopenshell.geom.utils.initialize_display()
+    for boundary in (b for b in space_boundaries if b.Name == "2ndLevel"):
+        # Retrieve boundary wire
+        geom = ifcopenshell.geom.create_shape(
+            settings, boundary.ConnectionGeometry.SurfaceOnRelatingElement.OuterBoundary
+        )  # type: OCC.TopoDS.Compound
+
+        wire = get_wire(geom)
+
+        plane = get_plane(boundary)
+
+        # Create boundary face
+        face = OCC.BRepBuilderAPI.BRepBuilderAPI_MakeFace(plane, wire).Face()
+
+        # Display boundary face
+        ifcopenshell.geom.utils.display_shape(face)
+
+    occ_display.FitAll()
+
+    input("Press any key")
+
+
+def get_wire(geom):
+    """Retrieve boundaries surface wire"""
+    ifc_verts = geom.verts
+    occ_verts = [
+        OCC.gp.gp_Pnt(ifc_verts[i], ifc_verts[i + 1], ifc_verts[i + 2])
+        for i in range(0, len(ifc_verts), 3)
+    ]
+    edges = [
+        OCC.BRepBuilderAPI.BRepBuilderAPI_MakeEdge(
+            occ_verts[i], occ_verts[i + 1]
+        ).Edge()
+        for i in range(0, len(occ_verts), 2)
+    ]
+    wire_maker = OCC.BRepBuilderAPI.BRepBuilderAPI_MakeWire()
     for edge in edges:
-        w.Add(edge)
-    wire = w.Wire()    
+        wire_maker.Add(edge)
+    return wire_maker.Wire()
 
-    # Retrieve boundary plane
-    position = boundary.ConnectionGeometry.SurfaceOnRelatingElement.BasisSurface.Position
-    c = position.Location.Coordinates
-    d = position.RefDirection.DirectionRatios
-    location = OCC.gp.gp_Pnt(c[0], c[1], c[2])
-    direction = OCC.gp.gp_Dir(d[0], d[1], d[2])
-    plane = OCC.gp.gp_Pln(location, direction)
 
-    # Create boundary face
-    face = OCC.BRepBuilderAPI.BRepBuilderAPI_MakeFace(plane, wire).Face()
-    
-    # Display boundary face
-    ifcopenshell.geom.utils.display_shape(face)
+def get_plane(boundary):
+    """Retrieve the plane boundary is built on"""
+    position = (
+        boundary.ConnectionGeometry.SurfaceOnRelatingElement.BasisSurface.Position
+    )
+    coordinates = position.Location.Coordinates
+    direction = position.RefDirection.DirectionRatios
+    location = OCC.gp.gp_Pnt(coordinates[0], coordinates[1], coordinates[2])
+    direction = OCC.gp.gp_Dir(direction[0], direction[1], direction[2])
+    return OCC.gp.gp_Pln(location, direction)
 
-input("Press any key")
+
+if __name__ == "__main__":
+    display_boundaries(ifc_path="IfcPython/9000_BIMxBEM_TestModèle_ACAD.ifc")
--- IfcPython/freecad_display_boundaries.py
+++ IfcPython/freecad_display_boundaries.py
@@ -1,60 +1,73 @@
 # coding: utf8
-# import ptvsd
-
-# # Allow other computers to attach to ptvsd at this IP address and port.
-# ptvsd.enable_attach(address=('localhost', 5678), redirect_output=True)
-
-# # Pause the program until a remote debugger is attached
-# ptvsd.wait_for_attach()
-# # breakpoint()
-
-import FreeCAD
 import ifcopenshell
 import ifcopenshell.geom
-import importIFC
-from FreeCAD import Part
+import OCC.BRep
+import OCC.BRepTools
+import OCC.TopoDS
+import ptvsd
 
-ifc_path = "/home/cyril/Projects/BIMxBEM/IfcPython/9000_BIMxBEM_TestModèle_ACAD.ifc"
-ifc_file = ifcopenshell.open(ifc_path)
-s = ifcopenshell.geom.settings()
-s.set(s.USE_PYTHON_OPENCASCADE, True)
-s.set(s.USE_WORLD_COORDS, True)
-s.set(s.INCLUDE_CURVES, True)
-s.set(s.USE_BREP_DATA, True)
-space_boundaries = ifc_file.by_type('IfcRelSpaceBoundary')
+import FreeCAD
+import Part
 
-doc = FreeCAD.ActiveDocument
 
-vectorize = lambda x:FreeCAD.Vector(x[0], x[1])
+def display_boundaries(ifc_path):
+    ifc_file = ifcopenshell.open(ifc_path)
+    settings = ifcopenshell.geom.settings()
+    settings.set(settings.USE_PYTHON_OPENCASCADE, True)
+    settings.set(settings.USE_WORLD_COORDS, True)
+    settings.set(settings.INCLUDE_CURVES, True)
+    settings.set(settings.USE_BREP_DATA, True)
+    space_boundaries = ifc_file.by_type("IfcRelSpaceBoundary")
 
-for space_boundary in (b for b in space_boundaries if b.Name == "2ndLevel"):
-    outer_boundary = space_boundary.ConnectionGeometry.SurfaceOnRelatingElement.OuterBoundary
-    # basis_surface = space_boundary.ConnectionGeometry.SurfaceOnRelatingElement.BasisSurface
+    doc = FreeCAD.ActiveDocument
 
-    # Create face
-    s=Part.makeBox(10,10,10)
-    if outer_boundary.is_a("IfcCompositeCurve"):
-        ifc_points = outer_boundary.Segments[0].ParentCurve.Points
-    elif outer_boundary.is_a("IfcPolyline"):
-        ifc_points = outer_boundary.Points
-    else:
-        print(f"Create Face: {outer_boundary.is_a()} not handled")
-        continue
-    fc_points = [vectorize(p.Coordinates) for p in ifc_points]
-    polygon = Part.makePolygon(fc_points)
-    face = Part.Face(polygon)
+    for space_boundary in (b for b in space_boundaries if b.Name == "2ndLevel"):
+        outer_boundary = (
+            space_boundary.ConnectionGeometry.SurfaceOnRelatingElement.OuterBoundary
+        )
 
-    # occ_shape = ifcopenshell.geom.create_shape(s, outer_boundary)
-    # face = Part.__fromPythonOCC__(occ_shape)
+        occ_shape = ifcopenshell.geom.create_shape(settings, outer_boundary)
+        tmp_file = "/tmp/shape.brep"
+        OCC.BRepTools.breptools_Write(occ_shape, tmp_file)
+        shape = Part.read(tmp_file)
+        face = doc.addObject("Part::Feature", "Face")
+        face.Shape = Part.Face(shape.Wires)
+        face.Placement = get_placement(space_boundary)
+        face.ViewObject.ShapeColor = get_color(space_boundary.RelatedBuildingElement)
 
-    # Translate face to its actual position
-    # position = outer_boundary.BasisSurface.Position
-    # matrix = FreeCAD.Matrix()
-    # matrix.move(FreeCAD.Vector(position.Location.Coordinates))
-    # matrix.rotate()
-    # position.Location
+    doc.recompute()
 
-    # Display result
-    Part.show(face)
 
-doc.recompute()
+def get_placement(boundary):
+    """Retrieve the plane boundary placement"""
+    position = (
+        boundary.ConnectionGeometry.SurfaceOnRelatingElement.BasisSurface.Position
+    )
+    base = FreeCAD.Vector(position.Location.Coordinates)
+    rotation = FreeCAD.Rotation(
+        FreeCAD.Vector(1, 0, 0), FreeCAD.Vector(position.RefDirection.DirectionRatios)
+    )
+    return FreeCAD.Placement(base, rotation)
+
+
+def get_color(ifc_product):
+    product_colors = {
+        "IfcWall": (0.7, 0.3, 0.0),
+        "IfcWindow": (0.0, 0.7, 1.0),
+        "IfcSlab": (0.7, 0.7, 0.5),
+        "IfcRoof": (0.0, 0.3, 0.0),
+    }
+    for product, color in product_colors.items():
+        if ifc_product.is_a(product):
+            return color
+
+
+if __name__ == "__main__":
+    # Allow other computers to attach to ptvsd at this IP address and port.
+    ptvsd.enable_attach(address=("localhost", 5678), redirect_output=True)
+    # Pause the program until a remote debugger is attached
+    ptvsd.wait_for_attach()
+    # breakpoint()
+    display_boundaries(
+        ifc_path="/home/cyril/Projects/BIMxBEM/IfcPython/9000_BIMxBEM_TestModèle_ACAD.ifc"
+    )
