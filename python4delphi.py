244e574abf57ac0d5ec607a972dccaa7ed7f2df3
Cyril Waechter <cyrwae@hotmail.com>
2020-04-11 18:18:18 +0200

    Correct noGui execution of python4delphi

--- python4delphi.py
+++ python4delphi.py
@@ -1,16 +1,16 @@
 import sys
-from PySide2 import QtCore, QtGui
-import FreeCADGui
-
 sys.path.append("Mod/BIMxBEM")
 from freecad.bem.boundaries import generate_bem_xml_from_file
 
-GUI_UP = True
+GUI_UP = False
 
 if GUI_UP:
+    from PySide2 import QtCore, QtGui
+    import FreeCADGui
     FreeCADGui.showMainWindow()
 
 xml_str = generate_bem_xml_from_file(r"C:\git\BIMxBEM\IfcTestFiles\2Storey_2x3_A22.ifc", GUI_UP)
+print(xml_str)
 
 if GUI_UP:
     FreeCADGui.activeView().viewIsometric()
